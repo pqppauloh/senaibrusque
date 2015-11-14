79ce7b208154f1226542ece6b727198ccd4896bc
Aluno <Aluno@10.1.20.62>
2015-11-13 22:56:07 -0200

    Começo do Bootstrap

--- index.php
+++ index.php
@@ -3,10 +3,33 @@
     <head>
         <meta charset="UTF-8">
         <title>Site Institucional do Senai - Brusque</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/estilo.css"/>
+
+        <!-- Javascript-->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.js" ></script>
+        <script src="js/meuscript.js"></script>
+        <link rel="shortcut icon" href="http://sc.senai.br/sites/default/files/favicon.ico" type="image/vnd.microsoft.icon">
     </head>
     <body>
-        <?php
-        echo "<h3>Pauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulo</h3>"
-        ?>
+        <div class="container-fluid">
+            <header class="jumbotron text-center" id="topo">
+                <h1>SENAI/SC Brusque</h1>
+                <form role="form">
+                     <div class="form-inline">
+                    <div class="input-group input-group-sm">
+                        <span class="input-group-addon ">
+                            <span class="glyphicon glyphicon-envelope"> </span>
+                        </span>
+                       
+                            <input type="email" class="form-control" id="email" placeholder="Digite seu E-mail" >
+                        </div>
+                        <button type="submit" class="btn btn-success">Cadastrar</button>
+                    </div>
+            </header>
+        </div>   
     </body>
 </html>
