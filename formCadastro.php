f74426e7682f9ce4aceebcc40364a8351e0ae613
pqpauloh <paulohsilvaoficial@hotmail.com>
2015-11-27 19:56:46 -0200

    Create formCadastro.php

---
+++ formCadastro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <header class="jumbotron text-center" id="topo">
+        <h1>SENAI/SC Brusque</h1>
+        <p>Alo Ha mundo do Bootstrap</p>
+
+
+
+
+        <body>
+            <form role="form" class="form-inline" action="cadastro.php" method="post">
+                <!-- Gerador de Bootstrap Form 
+                     http://embed.plnkr.co/ZrnnkFR3Yv9HGQHrRMiW/preview -->
+                <!-- Em caso de sucesso na validação: <div class="form-group has-feedback has-success"> -->
+                <!-- Em caso de falha na validação: <div class="form-group has-feedback has-error"> -->
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="seunome@provedor.com.br" size="40px" name="email"/>
+                    <i class="form-control-feedback glyphicon glyphicon-envelope"></i>
+                </div>
+                <button type="submit" class="btn btn-success" name="btn">Cadastrar</button>
+            </form>
+    </header>
+</body>
+</html>
