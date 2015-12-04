56f7500b68b46fea02ee2ca3fc96f03152f59a20
Aluno <Aluno@10.1.20.62>
2015-12-04 06:49:39 -0200

    Atualizando,saindo da Fase Alfa parte 2 para Fase BETA parte 1

---
+++ cursos.php
@@ -0,0 +1,140 @@
+<section id="cursos" class="container-fluid">
+    <h1>Cursos</h1>
+    <div class="row"><!-- Linha 1-->
+        <!-- Painel de Notícia 1-->
+        <div class="panel panel-success col-lg-3 col-md-3 col-sm-3">
+            <div class="panel-heading">Título do curso 1</div>
+            <div class="panel-body">Resumo do curso 1...</div>
+
+            <!-- botão que dispara a modal -->
+            <button type="button" class="btn btn-success center" data-toggle="modal" data-target="#curso1">leia mais...</button>
+
+            <!-- Modal -->
+            <div id="curso1" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+                    <!-- Conteúdo da modal-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Título do curso 1</h4>
+                        </div>
+                        <div class="modal-body">
+                            <time class="h4">01/12/2015 20:44:15</time>
+                            
+
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed tortor ac metus iaculis tristique nec a mi. Praesent fermentum a ante non fermentum. Cras lacus odio, laoreet at rutrum blandit, luctus eget sem. Curabitur malesuada erat ut justo sagittis, in iaculis sem varius. Donec gravida est non ex rhoncus molestie non eget sem. Maecenas quis molestie augue. Nullam auctor diam eget augue commodo, ac luctus nulla dictum. Maecenas congue, diam ac auctor tincidunt, ipsum erat finibus dui, eget euismod magna ipsum non ex. Aliquam elementum odio odio, ut tempus leo congue non. Vivamus tincidunt elementum urna, non sagittis erat facilisis a. Sed a arcu ut turpis consequat tincidunt et quis mauris. Nunc sed lacus augue.</p>
+
+                            <p>Phasellus eget sem ac nulla placerat pharetra. Proin a turpis pulvinar, pretium est non, gravida magna. Morbi ornare commodo faucibus. Donec consectetur dolor ut ex mollis, sed condimentum mi commodo. Ut et tempor tortor. Phasellus odio nisl, commodo eu mi et, lacinia efficitur mi. Donec efficitur, sapien vitae vestibulum varius, orci odio accumsan tortor, a suscipit quam ipsum id dui. Vestibulum ornare sapien gravida, pulvinar quam id, maximus justo. Nam sit amet ultrices dui, in gravida justo. Vestibulum maximus leo in nibh pellentesque, ut blandit eros tristique. Proin ac neque ut nunc luctus gravida. Donec dictum posuere orci, vel placerat justo suscipit viverra. Donec ac urna commodo, tempus lectus quis, porttitor ipsum. Nullam mi eros, gravida vitae mauris vitae, tincidunt tincidunt augue. Duis euismod neque in lectus iaculis, vestibulum ultrices ante malesuada. Ut posuere vel dolor sit amet finibus.</p>
+
+                            <p>Donec sit amet ex consequat, euismod magna eget, tempus velit. Mauris tincidunt eu mauris non luctus. Curabitur consequat eleifend molestie. Maecenas sollicitudin tincidunt eros, id placerat velit consectetur vel. Nam at ligula luctus neque sagittis consequat sed in odio. In et felis eu lorem dapibus elementum. Etiam nulla nisl, ullamcorper in ex in, dapibus volutpat lacus. Etiam non quam vulputate, vulputate ex in, interdum felis. Morbi tincidunt erat eget vulputate tristique. Proin nibh nibh, pharetra at posuere sed, venenatis ullamcorper enim. Suspendisse potenti.</p>
+
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec blandit bibendum euismod. Donec venenatis lacus eget ante ullamcorper vestibulum. Sed orci ligula, viverra vel semper id, laoreet non nunc. Donec ac nisl est. Sed venenatis nulla risus, nec dignissim dolor tristique a. Ut metus nibh, malesuada sit amet consequat ac, mollis vel lectus. Vivamus at elementum ante, laoreet lobortis tortor. Fusce tempus lacus vulputate sem euismod porttitor. Praesent pellentesque facilisis euismod.</p>
+
+                            <p>Aenean lorem justo, interdum vel nisl non, volutpat tempor purus. Curabitur tristique turpis sed cursus dictum. Praesent orci ligula, luctus ac dolor et, volutpat imperdiet odio. Donec in odio molestie, auctor tellus eu, ullamcorper sapien. Phasellus luctus ex vitae volutpat facilisis. Proin lectus nisl, sagittis at est id, suscipit ullamcorper sem. Pellentesque tempus vulputate orci. Aliquam non dictum risus. Quisque egestas vestibulum lectus id laoreet. Fusce felis tellus, vehicula quis erat eget, dictum interdum dui. Suspendisse commodo blandit gravida. </p>
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Fim da Modal -->
+        </div><!-- Painel notícia 1-->
+        
+        <!-- Painel de Notícia 2-->
+        <div class="panel panel-success col-lg-3 col-md-3 col-sm-3">
+            <div class="panel-heading">Título do curso 2</div>
+            <div class="panel-body">Resumo do curso 2...</div>
+
+            <!-- botão que dispara a modal -->
+            <button type="button" class="btn btn-success center" data-toggle="modal" data-target="#curso2">leia mais...</button>
+
+            <!-- Modal -->
+            <div id="curso2" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+                    <!-- Conteúdo da modal-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Título do Curso 2</h4>
+                        </div>
+                        <div class="modal-body">
+                            <time class="h4">30/11/2015 19:30:45</time>
+                            
+                            <h3>Lorem Ipsum 2</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed tortor ac metus iaculis tristique nec a mi. Praesent fermentum a ante non fermentum. Cras lacus odio, laoreet at rutrum blandit, luctus eget sem. Curabitur malesuada erat ut justo sagittis, in iaculis sem varius. Donec gravida est non ex rhoncus molestie non eget sem. Maecenas quis molestie augue. Nullam auctor diam eget augue commodo, ac luctus nulla dictum. Maecenas congue, diam ac auctor tincidunt, ipsum erat finibus dui, eget euismod magna ipsum non ex. Aliquam elementum odio odio, ut tempus leo congue non. Vivamus tincidunt elementum urna, non sagittis erat facilisis a. Sed a arcu ut turpis consequat tincidunt et quis mauris. Nunc sed lacus augue.</p>
+
+                            <p>Phasellus eget sem ac nulla placerat pharetra. Proin a turpis pulvinar, pretium est non, gravida magna. Morbi ornare commodo faucibus. Donec consectetur dolor ut ex mollis, sed condimentum mi commodo. Ut et tempor tortor. Phasellus odio nisl, commodo eu mi et, lacinia efficitur mi. Donec efficitur, sapien vitae vestibulum varius, orci odio accumsan tortor, a suscipit quam ipsum id dui. Vestibulum ornare sapien gravida, pulvinar quam id, maximus justo. Nam sit amet ultrices dui, in gravida justo. Vestibulum maximus leo in nibh pellentesque, ut blandit eros tristique. Proin ac neque ut nunc luctus gravida. Donec dictum posuere orci, vel placerat justo suscipit viverra. Donec ac urna commodo, tempus lectus quis, porttitor ipsum. Nullam mi eros, gravida vitae mauris vitae, tincidunt tincidunt augue. Duis euismod neque in lectus iaculis, vestibulum ultrices ante malesuada. Ut posuere vel dolor sit amet finibus.</p>
+
+                            <p>Donec sit amet ex consequat, euismod magna eget, tempus velit. Mauris tincidunt eu mauris non luctus. Curabitur consequat eleifend molestie. Maecenas sollicitudin tincidunt eros, id placerat velit consectetur vel. Nam at ligula luctus neque sagittis consequat sed in odio. In et felis eu lorem dapibus elementum. Etiam nulla nisl, ullamcorper in ex in, dapibus volutpat lacus. Etiam non quam vulputate, vulputate ex in, interdum felis. Morbi tincidunt erat eget vulputate tristique. Proin nibh nibh, pharetra at posuere sed, venenatis ullamcorper enim. Suspendisse potenti.</p>
+
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec blandit bibendum euismod. Donec venenatis lacus eget ante ullamcorper vestibulum. Sed orci ligula, viverra vel semper id, laoreet non nunc. Donec ac nisl est. Sed venenatis nulla risus, nec dignissim dolor tristique a. Ut metus nibh, malesuada sit amet consequat ac, mollis vel lectus. Vivamus at elementum ante, laoreet lobortis tortor. Fusce tempus lacus vulputate sem euismod porttitor. Praesent pellentesque facilisis euismod.</p>
+
+                            <p>Aenean lorem justo, interdum vel nisl non, volutpat tempor purus. Curabitur tristique turpis sed cursus dictum. Praesent orci ligula, luctus ac dolor et, volutpat imperdiet odio. Donec in odio molestie, auctor tellus eu, ullamcorper sapien. Phasellus luctus ex vitae volutpat facilisis. Proin lectus nisl, sagittis at est id, suscipit ullamcorper sem. Pellentesque tempus vulputate orci. Aliquam non dictum risus. Quisque egestas vestibulum lectus id laoreet. Fusce felis tellus, vehicula quis erat eget, dictum interdum dui. Suspendisse commodo blandit gravida. </p>
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Fim da Modal -->
+        </div><!-- Painel notícia 2-->
+
+        <!-- Painel de Notícia 3-->
+        <div class="panel panel-success col-lg-3 col-md-3 col-sm-3">
+            <div class="panel-heading">Título do curso 3</div>
+            <div class="panel-body">Resumo do curso 3...</div>
+
+            <!-- botão que dispara a modal -->
+            <button type="button" class="btn btn-success center" data-toggle="modal" data-target="#curso3">leia mais...</button>
+
+            <!-- Modal -->
+            <div id="curso3" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+                    <!-- Conteúdo da modal-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Título do curso 3</h4>
+                        </div>
+                        <div class="modal-body">
+                            <time class="h4">25/10/2015 10:20:05</time>
+                           
+                            <h3>Lorem Ipsum 3</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed tortor ac metus iaculis tristique nec a mi. Praesent fermentum a ante non fermentum. Cras lacus odio, laoreet at rutrum blandit, luctus eget sem. Curabitur malesuada erat ut justo sagittis, in iaculis sem varius. Donec gravida est non ex rhoncus molestie non eget sem. Maecenas quis molestie augue. Nullam auctor diam eget augue commodo, ac luctus nulla dictum. Maecenas congue, diam ac auctor tincidunt, ipsum erat finibus dui, eget euismod magna ipsum non ex. Aliquam elementum odio odio, ut tempus leo congue non. Vivamus tincidunt elementum urna, non sagittis erat facilisis a. Sed a arcu ut turpis consequat tincidunt et quis mauris. Nunc sed lacus augue.</p>
+
+                            <p>Phasellus eget sem ac nulla placerat pharetra. Proin a turpis pulvinar, pretium est non, gravida magna. Morbi ornare commodo faucibus. Donec consectetur dolor ut ex mollis, sed condimentum mi commodo. Ut et tempor tortor. Phasellus odio nisl, commodo eu mi et, lacinia efficitur mi. Donec efficitur, sapien vitae vestibulum varius, orci odio accumsan tortor, a suscipit quam ipsum id dui. Vestibulum ornare sapien gravida, pulvinar quam id, maximus justo. Nam sit amet ultrices dui, in gravida justo. Vestibulum maximus leo in nibh pellentesque, ut blandit eros tristique. Proin ac neque ut nunc luctus gravida. Donec dictum posuere orci, vel placerat justo suscipit viverra. Donec ac urna commodo, tempus lectus quis, porttitor ipsum. Nullam mi eros, gravida vitae mauris vitae, tincidunt tincidunt augue. Duis euismod neque in lectus iaculis, vestibulum ultrices ante malesuada. Ut posuere vel dolor sit amet finibus.</p>
+
+                            <p>Donec sit amet ex consequat, euismod magna eget, tempus velit. Mauris tincidunt eu mauris non luctus. Curabitur consequat eleifend molestie. Maecenas sollicitudin tincidunt eros, id placerat velit consectetur vel. Nam at ligula luctus neque sagittis consequat sed in odio. In et felis eu lorem dapibus elementum. Etiam nulla nisl, ullamcorper in ex in, dapibus volutpat lacus. Etiam non quam vulputate, vulputate ex in, interdum felis. Morbi tincidunt erat eget vulputate tristique. Proin nibh nibh, pharetra at posuere sed, venenatis ullamcorper enim. Suspendisse potenti.</p>
+
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec blandit bibendum euismod. Donec venenatis lacus eget ante ullamcorper vestibulum. Sed orci ligula, viverra vel semper id, laoreet non nunc. Donec ac nisl est. Sed venenatis nulla risus, nec dignissim dolor tristique a. Ut metus nibh, malesuada sit amet consequat ac, mollis vel lectus. Vivamus at elementum ante, laoreet lobortis tortor. Fusce tempus lacus vulputate sem euismod porttitor. Praesent pellentesque facilisis euismod.</p>
+
+                            <p>Aenean lorem justo, interdum vel nisl non, volutpat tempor purus. Curabitur tristique turpis sed cursus dictum. Praesent orci ligula, luctus ac dolor et, volutpat imperdiet odio. Donec in odio molestie, auctor tellus eu, ullamcorper sapien. Phasellus luctus ex vitae volutpat facilisis. Proin lectus nisl, sagittis at est id, suscipit ullamcorper sem. Pellentesque tempus vulputate orci. Aliquam non dictum risus. Quisque egestas vestibulum lectus id laoreet. Fusce felis tellus, vehicula quis erat eget, dictum interdum dui. Suspendisse commodo blandit gravida. </p>
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Fim da Modal -->
+        </div><!-- Painel notícia 3-->
+    </div>
+</section>
